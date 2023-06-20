2a9b780cdde35298e07950b2737d192169af7a72
Efim <efimmanevich@gmail.com>
2023-06-20 18:18:14 +0300

    CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from util import gethtml
@@ -6,9 +7,21 @@ import appauth
 import filemanager
 import reactrout
 
-
-
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(appauth.router)
 app.include_router(filemanager.router)
 app.include_router(reactrout.app)
